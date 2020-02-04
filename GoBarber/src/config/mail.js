1d58c6f9289ab34245b771bf0bed324c87c667bc
EnableZero <mottalucca@yahoo.com.br>
2020-02-03 21:52:52 -0300

    Notifications/Mailing feature

---
+++ GoBarber/src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+        host: "smtp.mailtrap.io",
+        port: 2525,
+        secure:false,
+        auth: {
+                user: "84d6993324c425",
+                pass: "6bba281a928a14"
+        },
+        default: {
+                from: 'Equipe GoBarber <noreply@gobarber.com>'
+        },
+};
